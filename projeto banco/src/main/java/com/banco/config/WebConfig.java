d09c3323f912915d2e05eb674f429771ce30f70e
Paulo-dev-98 <paulobg8@gmail.com>
2022-09-02 19:01:36 -0300

    adicionando documentação swagger

--- projeto banco/src/main/java/com/banco/config/WebConfig.java
+++ projeto banco/src/main/java/com/banco/config/WebConfig.java
@@ -3,13 +3,19 @@ package com.banco.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-@EnableWebMvc
-
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig extends WebMvcConfigurationSupport {
+	
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 	
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
